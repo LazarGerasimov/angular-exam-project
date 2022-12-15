7f93237b83c810c3dfbe4a4b0fa2761442843029
Laz <lazgerasimov@gmail.com>
2022-12-15 08:11:08 +0000

    api service

---
+++ src/app/api.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const apiUrl = environment.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+
+  // TODO add db requests
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,30 +1,23 @@
 export const environment = {
     production: false,
-    firebaseConfig: {
-        apiKey: "AIzaSyBNcxKpLRTukOpKkUOzEbWxfQDhzkI-RHY",
-        authDomain: "games-store-c039b.firebaseapp.com",
-        projectId: "games-store-c039b",
-        storageBucket: "games-store-c039b.appspot.com",
-        messagingSenderId: "797340755113",
-        appId: "1:797340755113:web:4efe0250010cdb59c635b8"
-      }
+    apiUrl: 'http://localhost:3000/'  
 };
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBNcxKpLRTukOpKkUOzEbWxfQDhzkI-RHY",
-  authDomain: "games-store-c039b.firebaseapp.com",
-  projectId: "games-store-c039b",
-  storageBucket: "games-store-c039b.appspot.com",
-  messagingSenderId: "797340755113",
-  appId: "1:797340755113:web:4efe0250010cdb59c635b8"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBNcxKpLRTukOpKkUOzEbWxfQDhzkI-RHY",
+//   authDomain: "games-store-c039b.firebaseapp.com",
+//   projectId: "games-store-c039b",
+//   storageBucket: "games-store-c039b.appspot.com",
+//   messagingSenderId: "797340755113",
+//   appId: "1:797340755113:web:4efe0250010cdb59c635b8"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
